2142be72b7a110634234e9f6c6630e01c00e0bd8
daphne12345 <daphne.theo@yahoo.de>
2024-08-22 10:56:06 +0200

    Bug fixing

--- deepcave/evaluators/mo_fanova.py
+++ deepcave/evaluators/mo_fanova.py
@@ -125,6 +125,7 @@ class MOfANOVA(fANOVA):
             df_res = pd.DataFrame(super(MOfANOVA, self).get_importances(hp_names=None)).loc[0:1].T.reset_index()
             df_res['weight_for_' + objectives_normed[0]] = w[0]
             df_all = pd.concat([df_all, df_res])
+        print(df_all)
         self.importances_ = df_all.rename(columns={0: 'importance', 1: 'variance', 'index': 'hp_name'})
         # importances_ = df_all.sort_values(by='weight_for_1-accuracy_normed').groupby('hp_name').agg(list).T.to_dict('list') # Dict[hp_name: [importances->list, variances->list, weightings->list]]
         # return importances_

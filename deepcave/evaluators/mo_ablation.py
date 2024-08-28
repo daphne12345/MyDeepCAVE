38a57742c65ce82c3f273200e23a6d7aabeab39c
daphne12345 <daphne.theo@yahoo.de>
2024-08-28 13:42:05 +0200

    Add MO Ablation Path

--- deepcave/evaluators/mo_ablation.py
+++ deepcave/evaluators/mo_ablation.py
@@ -206,8 +206,6 @@ class MOAblation(Ablation):
 
                 # Remove the current best hp for keeping the order right
                 hp_it.remove(max_hp)
-                print(df_abl)
-                print(df_abl.reset_index(drop=True))
             return df_abl.reset_index(drop=True)
 
     def _ablation(
--- deepcave/plugins/hyperparameter/ablation_paths.py
+++ deepcave/plugins/hyperparameter/ablation_paths.py
@@ -532,6 +532,7 @@ class AblationPaths(StaticPlugin):
         # Sort by last fidelity now
         idx = data[selected_budget_id].groupby("hp_name")['importance'].max().sort_values(ascending=False).index
         idx = idx[:n_hps] + ['Default']
+        print(idx)
 
         figure = go.Figure()
 
